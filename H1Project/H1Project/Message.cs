91fff68820d7bdc75ec272e2bbab6ecbde81e018
Sheep <Sheepsaurus@gmail.com>
2018-10-29 12:59:27 +0100

    Error Avoidance

--- H1Project/H1Project/Message.cs
+++ H1Project/H1Project/Message.cs
@@ -1,6 +1,13 @@
-﻿namespace H1Project
+﻿using System;
+
+namespace H1Project
 {
 	internal class Message
 	{
+		internal bool wasRecieved()
+		{
+			// Temporary implementation to avoid error message
+			throw new NotImplementedException();
+		}
 	}
 }
