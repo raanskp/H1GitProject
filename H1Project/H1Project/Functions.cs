f4442b998e8c030e0dcd9c59710110b099313ceb
Sheep <Sheepsaurus@gmail.com>
2018-10-29 12:20:23 +0100

    Added Functions.cs and declared initial functions
    "Start" and "Quit" are both examplary and initial

--- H1Project/H1Project/Functions.cs
+++ H1Project/H1Project/Functions.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace H1Project
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
-    }
+	public class Functions
+	{
+		public void Quit()
+		{
+
+		}
+
+		public void Start()
+		{
+
+		}
+	}
 }
