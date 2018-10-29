106a5d3c6a1e37b4da5bcf3510f8623ae32f7016
Claes Holmvang <claes_holmvang@hotmail.com>
2018-10-29 13:42:22 +0100

    Input
    The user starts and stops the program

--- H1Project/H1Project/Program.cs
+++ H1Project/H1Project/Program.cs
@@ -14,8 +14,21 @@ namespace H1Project
 
 			while (true)
 			{
-				//Input
-				functions.HandleCommands("input");
+                Console.WriteLine("Hello and welcome to this chat bot.");
+                Console.WriteLine("If you would like to chat, type Y for yes, and N for no");
+
+                string userValue = Console.ReadLine();
+
+                if (userValue.ToLower() == "Y")
+                {
+
+                }
+
+                if (userValue.ToLower() == "N")
+                {
+
+                }
+                functions.HandleCommands("input");
 			}
         }
     }
