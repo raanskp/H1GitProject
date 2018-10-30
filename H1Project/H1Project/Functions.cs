cfcc48a72fca6625cedcac9a0e5b02e119911812
Claes Holmvang <claes_holmvang@hotmail.com>
2018-10-30 08:52:57 +0100

    Print bot answers
    Here the program prints the bot answers

--- H1Project/H1Project/Functions.cs
+++ H1Project/H1Project/Functions.cs
@@ -114,8 +114,12 @@ namespace H1Project
 		private void Start()
 		{
             // Introduction to the bot, functionality, commands, experience. - Probably redundant.
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Claes\Documents\GitHub\H1GitProject\H1Project\H1Project\bin\Debug\BotSvar.txt");
-
+            string[] lines = System.IO.File.ReadAllLines(@"BotSvar.txt");
+            System.Console.WriteLine("Contents of BotSvar.txt =");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
         }
 
 		public void HandleCommands(string input)
